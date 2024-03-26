220016a7fb5e57a88d420fe299d3b0ff8d5d4321
Silambarasan <silambarasanbabu73@gmail.com>
2024-03-26 11:13:28 +0530

    EventDelegation

---
+++ 28EventDelegation.js
@@ -0,0 +1,11 @@
+document.getElementById("parent").addEventListener("click", (e) => {
+  let path = e.target.id;
+  if (e.target.tagName === "LI") window.location.href = "/" + path;
+});
+
+document.getElementById("form").addEventListener("keyup", (e) => {
+  console.log(e);
+  if (e.target.dataset.uppercase != undefined) {
+    e.target.value = e.target.value.toUpperCase();
+  }
+});
